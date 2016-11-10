eac2f3f77c71b566b8ea8a0e89d12bd4c8d42755
lelefly <18501250210@163.com>
2016-11-10 16:48:35 +0800

    done

--- app/src/main/java/com/le/star/MainActivity.java
+++ app/src/main/java/com/le/star/MainActivity.java
@@ -3,6 +3,7 @@ package com.le.star;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,14 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final StarRatingView starRatingView = (StarRatingView) findViewById(R.id.srv);
+        final StarRatingView srv = (StarRatingView) findViewById(R.id.srv);
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                starRatingView.setRate(count);
                 count++;
+                srv.setRate(count);
             }
         });
-
+        final TextView tv = (TextView) findViewById(R.id.tv);
+        StarRatingView srv_ratable = (StarRatingView) findViewById(R.id.srv_ratable);
+        srv_ratable.setOnRateChangeListener(new StarRatingView.OnRateChangeListener() {
+            @Override
+            public void onRateChange(int rate) {
+                tv.setText(rate+"分");
+            }
+        });
     }
+
 }
--- app/src/main/java/com/le/star/StarRatingView.java
+++ app/src/main/java/com/le/star/StarRatingView.java
@@ -4,23 +4,25 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Administrator on 2016/11/9.
+ * Created by le on 2016/11/9.
  */
 public class StarRatingView extends LinearLayout{
-    private Drawable on,off,half;
-    private boolean ratable;
-    private float padding;
-    private List<ImageView> list;
+    private Drawable on,off,half;   //星星三种图片
+    private boolean ratable;    //是否点击滑动
+    private float padding;  //星星之间间距
+    private List<ImageView> list;   //存放五颗星星
+    private float  points[] = new float[11];    //0-10分点的x坐标，index为分数，值为此分数为坐标
+    private float starWidth;    //设置的星星图片宽度
+    private float halfStarWidth;    //半个星星宽度
+    private int paddingLeft;       //控件左padding
+    private OnRateChangeListener onRateChangeListener;
 
     public StarRatingView(Context context) {
         super(context);
@@ -33,20 +35,31 @@ public class StarRatingView extends LinearLayout{
         off = array.getDrawable(R.styleable.StarRatingView_stat_off);
         half = array.getDrawable(R.styleable.StarRatingView_stat_half);
         ratable = array.getBoolean(R.styleable.StarRatingView_ratable, false);
-        padding = array.getDimension(R.styleable.StarRatingView_star_padding, on.getIntrinsicWidth() / 3);
+        halfStarWidth = on.getIntrinsicWidth() / 2;
+        starWidth = on.getIntrinsicWidth();
+        padding = array.getDimension(R.styleable.StarRatingView_star_padding, starWidth / 3);
+        paddingLeft = getPaddingLeft();
         list = new ArrayList<>();
         ImageView imageView;
+        //初始化五个星星，并通过points数组把分数与坐标对应起来
         for(int i=0;i<5;i++){
             imageView = new ImageView(context);
             LinearLayout.LayoutParams layoutParams = new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
             if(i!=0&&i!=4) {
                 layoutParams.setMargins((int) padding / 2, 0, (int) padding / 2, 0);
+                points[i*2] = i*starWidth+i*padding;
+                points[i*2+1] = points[i*2]+halfStarWidth/2;
             }
             if(i==0){
                 layoutParams.setMargins(0, 0, (int) padding / 2, 0);
+                points[0] = 0;
+                points[1] = halfStarWidth;
             }
             if(i==4){
                 layoutParams.setMargins((int) padding / 2, 0, 0, 0);
+                points[i*2] = i*starWidth+i*padding;
+                points[i*2+1] = points[i*2]+halfStarWidth/2;
+                points[10] = points[9]+halfStarWidth/2;
             }
             imageView.setLayoutParams(layoutParams);
             imageView.setImageDrawable(off);
@@ -54,8 +67,13 @@ public class StarRatingView extends LinearLayout{
             addView(list.get(i));
         }
         setOrientation(LinearLayout.HORIZONTAL);
+        array.recycle();
     }
 
+    /**
+     *根据分数显示星星
+     * @param rate
+     */
     public void setRate(int rate){
         removeAllViews();
         int count = rate/2;
@@ -76,10 +94,47 @@ public class StarRatingView extends LinearLayout{
             }
             addView(list.get(i));
         }
+        if(ratable&&onRateChangeListener!=null){
+            onRateChangeListener.onRateChange(rate);
+        }
+    }
+
+    public void setOnRateChangeListener(OnRateChangeListener l){
+        onRateChangeListener = l;
+
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        return super.onTouchEvent(event);
+        //设置不可以滑动则不处理事件
+        if(!ratable){
+            return super.onTouchEvent(event);
+        }
+        setRate(calculateStar(event.getX()));
+        return true;
+    }
+
+    /**
+     * 根据坐标计算分数
+     * @param x
+     * @return
+     */
+    private int calculateStar(float x) {
+        //减去控件左padding
+        float realPosition = x-paddingLeft;
+        for(int i = 0;i<points.length;i++){
+            if(points[i]>realPosition){
+                return i;
+            }
+        }
+        //如果循环结束即为右滑超出最大值，返回10分
+        return 10;
+    }
+
+    /**
+     * 评分改变的回调
+     */
+    public interface OnRateChangeListener{
+        void onRateChange(int rate);
     }
 }
